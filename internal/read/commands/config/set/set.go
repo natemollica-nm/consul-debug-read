bb020a6446565d7b043c1a379ceb52707e06b9c2
natemollica-dev <nathan.mollica@hashicorp.com>
2024-02-12 08:57:11 -0800

    set command: updated older debug build metrics.json merging to go instead of script | updated to include consul.log verification for consul log parse command

--- internal/read/commands/config/set/set.go
+++ internal/read/commands/config/set/set.go
@@ -9,8 +9,9 @@ import (
 	"github.com/hashicorp/go-hclog"
 	"github.com/mitchellh/cli"
 	"gopkg.in/yaml.v2"
+	"io"
 	"os"
-	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
@@ -146,7 +147,7 @@ func UpdateDebugReadConfig(updatePath string) (bool, error) {
 }
 
 const synopsis = `Changes which bundle you're focusing on for analysis`
-const setDebugPathHelp = `consul-debug-read set [options]
+const setDebugPathHelp = `consul-debug-read config set [options]
 
 Validates the path contents or extracts a valid .tar.gz bundle and points to this valid directory path for processing.
 
@@ -159,10 +160,10 @@ Validates the path contents or extracts a valid .tar.gz bundle and points to thi
   * path to multiple bundles available for extraction and path setting
 
 Example (-path):
-	$ consul-debug-read set -path bundles/consul-debug-2023-10-04T18-29-47Z
+	$ consul-debug-read config set -path bundles/consul-debug-2023-10-04T18-29-47Z
 
 Example (-path) for dir containing multiple .tar.gz bundles:
-	$ consul-debug-read set -path bundles
+	$ consul-debug-read config set -path bundles/
 
 	select a .tar.gz file to extract:
 	1: 124722consul-debug-2023-10-04T18-29-47Z.tar.gz
@@ -175,7 +176,7 @@ Example (-path) for dir containing multiple .tar.gz bundles:
 	enter the number of the file to extract: 
 
 Example (-file) for extraction:
-	$ consul-debug-read set -file bundles/124722consul-debug-2023-10-11T17-43-15Z.tar.gz
+	$ consul-debug-read config set-path -file bundles/124722consul-debug-2023-10-11T17-43-15Z.tar.gz
 `
 
 func ValidateDebugPath(path string) (bool, error) {
@@ -190,7 +191,7 @@ func ValidateDebugPath(path string) (bool, error) {
 		return false, err
 	}
 
-	var metricsJson, agentJson, membersJson, hostJson, indexJson bool
+	var metricsJson, agentJson, membersJson, hostJson, indexJson, consulLog bool
 	for _, file := range entries {
 		if file.IsDir() {
 			continue
@@ -206,6 +207,8 @@ func ValidateDebugPath(path string) (bool, error) {
 			indexJson = true
 		case "members.json":
 			membersJson = true
+		case "consul.log":
+			consulLog = true
 		case "cluster.json":
 			clusterJsonPath := path + "/" + file.Name()
 			membersJsonPath := path + "/members.json"
@@ -216,19 +219,140 @@ func ValidateDebugPath(path string) (bool, error) {
 		}
 	}
 	if agentJson && membersJson && hostJson && indexJson {
-		// older debug bundles separated v1/agent/metrics captures into each interval
-		// if so, try and merge the metrics.json files into one large metrics.json
-		// for ingestion.
 		if !metricsJson {
-			// "metrics.json" not found in the current directory
-			// Run the "merge-metrics.sh" script with debugPath as an argument
-			scriptPath := "scripts/merge-metrics.sh"
-			merge := exec.Command(scriptPath, path)
-			if _, err = merge.CombinedOutput(); err != nil {
+			err = ConcatenateMetrics(path)
+			if err != nil {
+				return false, err
+			}
+		}
+		if !consulLog {
+			err = RetrieveFirstConsulLog(path)
+			if err != nil {
 				return false, err
 			}
 		}
 		return true, nil
 	}
-	return false, fmt.Errorf("invalid path setting passed in | file-check: metrics=%v, agent=%v, host=%v, index=%v, members=%v", metricsJson, agentJson, hostJson, indexJson, membersJson)
+	return false, fmt.Errorf("invalid path setting passed in | file-check: metrics=%v, agent=%v, host=%v, index=%v, members=%v, log=%v", metricsJson, agentJson, hostJson, indexJson, membersJson, consulLog)
+}
+
+// ConcatenateMetrics reads all metrics.json files in the subdirectories of bundle
+// and appends their contents to a single metrics.json file in root bundle dir.
+//
+// Older debug bundles separated v1/agent/metrics captures into each interval
+// so in this case we try and merge the metrics.json files into one large metrics.json
+// for ingestion.
+func ConcatenateMetrics(path string) error {
+	var err error
+	// Open or create the final metrics.json file for appending
+	outputFile, err := os.OpenFile(filepath.Join(path, "metrics.json"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
+	if err != nil {
+		return fmt.Errorf("unable to open or create metrics.json file: %w", err)
+	}
+	defer outputFile.Close()
+	cleanup := func(err error) error {
+		_ = outputFile.Close()
+		return err
+	}
+
+	// Read directories in the debugPath
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return fmt.Errorf("error reading directory: %w", err)
+	}
+
+	for _, entry := range entries {
+		if entry.IsDir() {
+			captureDir := filepath.Join(path, entry.Name())
+			metricsPath := filepath.Join(captureDir, "metrics.json")
+
+			// Check if metrics.json exists in the directory
+			if _, err = os.Stat(metricsPath); err == nil {
+				var metricsData []byte
+				metricsData, err = os.ReadFile(metricsPath)
+				if err != nil {
+					return fmt.Errorf("error reading %v: %w", metricsPath, err)
+				}
+
+				// Append the metrics data to the final metrics.json file
+				_, err = outputFile.Write(metricsData)
+				if err != nil {
+					return fmt.Errorf("error writing to metrics.json: %w", err)
+				}
+
+				// Optionally, append a newline between entries
+				_, err = outputFile.WriteString("\n")
+				if err != nil {
+					return fmt.Errorf("error writing newline to metrics.json: %w", err)
+				}
+			}
+		}
+	}
+	if err = outputFile.Close(); err != nil {
+		return cleanup(err)
+	}
+
+	return nil
+}
+
+// RetrieveFirstConsulLog searches subdirectories of debug path for the first "consul.log"
+// and copies it to path root. If no "consul.log" is found, it returns an error.
+func RetrieveFirstConsulLog(path string) error {
+	found := false
+	var err error
+	var srcFile, dstFile *os.File
+
+	// Read directories in the debugPath
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return fmt.Errorf("error reading directory: %w", err)
+	}
+
+	cleanup := func(file *os.File, err error) error {
+		_ = file.Close()
+		return err
+	}
+
+	for _, entry := range entries {
+		if entry.IsDir() {
+			consulLogPath := filepath.Join(path, entry.Name(), "consul.log")
+
+			// Check if consul.log exists in the directory
+			if _, err = os.Stat(consulLogPath); err == nil {
+
+				// Copy consul.log to debugPath
+				srcFile, err = os.Open(consulLogPath)
+				if err != nil {
+					return fmt.Errorf("error opening source consul.log: %w", err)
+				}
+				defer srcFile.Close()
+
+				dstFile, err = os.Create(filepath.Join(path, "consul.log"))
+				if err != nil {
+					return fmt.Errorf("error creating destination consul.log: %w", err)
+				}
+				defer dstFile.Close()
+
+				if _, err = io.Copy(dstFile, srcFile); err != nil {
+					return fmt.Errorf("error copying consul.log: %w", err)
+				}
+
+				found = true
+				break // Stop searching after finding the first instance
+			}
+		}
+	}
+
+	if !found {
+		return fmt.Errorf("consul.log not found in any subdirectory")
+	}
+
+	if err = srcFile.Close(); err != nil {
+		return cleanup(srcFile, err)
+	}
+	if err = dstFile.Close(); err != nil {
+		return cleanup(dstFile, err)
+	}
+
+	return nil
 }
