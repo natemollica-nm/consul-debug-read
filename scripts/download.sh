6e696206b4f004fae31bf924b1de56f42d06b276
natemollica-dev <nathan.mollica@hashicorp.com>
2024-02-28 12:55:57 -0800

    download script GOPATH and sudo and pretty prints updates

--- scripts/download.sh
+++ scripts/download.sh
@@ -2,6 +2,11 @@
 
 set -eEo pipefail
 
+ARCH=$( [[ "$(uname -m)" =~ aarch64|arm64 ]] && echo arm64 || echo amd64)
+PLATFORM=$(uname | tr '[:upper:]' '[:lower:]')
+VERSION="$(curl -s https://api.github.com/repos/natemollica-nm/consul-debug-read/releases/latest | jq -r '.tag_name')"
+URL=https://github.com/natemollica-nm/consul-debug-read/releases/download/"${VERSION}"/consul-debug-read_"${VERSION}"_"${PLATFORM}"_"${ARCH}".tar.gz
+
 ################ Font Formatting #################
 ##################################################
 BOLD="\033[1m"; UNBOLD="\033[0m"; YELLOW="\e[93m";
@@ -20,11 +25,85 @@ function warn { >&2 printf '\n%b%s' "$(now) ${INTENSE_YELLOW}""${BOLD}"[WARN]"${
 # shellcheck disable=SC2145
 function err { >&2 printf '\n%b%s' "$(now) ${RED}""${BOLD}"[ERROR]"${UNBOLD}""${END_COLOR} " " $@"; EXIT_CODE=1 && echo && return "$EXIT_CODE"; }
 
-ARCH=$( [[ "$(uname -m)" =~ aarch64|arm64 ]] && echo arm64 || echo amd64)
-PLATFORM=$(uname | tr '[:upper:]' '[:lower:]')
-VERSION="$(curl -s https://api.github.com/repos/natemollica-nm/consul-debug-read/releases/latest | jq -r '.tag_name')"
-URL=https://github.com/natemollica-nm/consul-debug-read/releases/download/"${VERSION}"/consul-debug-read_"${VERSION}"_"${PLATFORM}"_"${ARCH}".tar.gz
 
+# Define an associative array with tool names as keys and GitHub repository | software URLs as values
+declare -A tools=(
+    ["wget"]="https://formulae.brew.sh/formula/wget"
+)
+
+function sudo_prompt() {
+  local attempt max_attempts auth
+  attempt=0
+  max_attempts=3
+
+  while true; do
+      # Prompt user for their sudo password with custom message
+      info "Please enter sudo password for installation: "
+      read -s -r auth && echo "$auth" | sudo -S -v -p ''
+      if [ $? -eq 0 ]; then
+          info "sudo authentication successful"
+          # Place your script commands that require sudo here
+          break
+      else
+          if [ $attempt -ge $max_attempts ]; then
+              err "sudo authentication failed. Maximum attempts reached!"
+              exit 1
+          else
+              warn "sudo authentication failed - please re-attempt sudo authentication password:"
+              ((attempt++))
+          fi
+      fi
+  done
+}
+
+function go_path_verification() {
+  # Check if GOPATH is set
+  if [ -z "$GOPATH" ]; then
+    info "GOPATH environment variable not set"
+    return 0 # Return as we cannot remove from unknown location
+  fi
+
+  # Construct the path to the binary
+  local binaryPath="$GOPATH/bin/consul-debug-read" response
+
+  # Check if the binary exists
+  if [ -f "$binaryPath" ]; then
+    # Prompt user for deletion
+    info "previous binary found in \$GOPATH/bin"
+    printf '\n\n%s\n' "    *==> Having '$GOPATH/bin/consul-debug-read' present can introduce conflicts when trying to run consul-debug-read."
+    printf '%s' "    *==> Do you want to delete it? (y/n): "
+    read -r response </dev/stdin
+
+    case $response in
+      [Yy]* )
+        rm -f "$binaryPath" || { return 1; } # Delete the binary
+        info "deleted consul-debug-read binary from $GOPATH/bin"
+        return 0
+        ;;
+      * )
+        info "skipping previous consul-debug-read binary deletion from $GOPATH/bin"
+        return 0
+        ;;
+    esac
+  else
+    info "binary $binaryPath does not found at $GOPATH/bin"
+  fi
+  return 0
+}
+
+function check_and_install_brew() {
+  # Check that brew is installed
+  if ! command -v brew >/dev/null 2>&1; then
+    # not installed
+    read -p "Brew not detected. Install now? (y/n)" -n 1 -r
+    echo
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+      [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+    fi
+
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+}
 trap cleanup EXIT TERM SIGINT
 function cleanup() {
   debug "install consul-debug-read: running cleaning up..."
@@ -34,8 +113,49 @@ function cleanup() {
   rm -rf /tmp/consul-debug-read >/dev/null || {
    debug "install consul-debug-read: failed to remove /tmp/consul-debug-read"
   }
-  info "install consul-debug-read: done"
+  debug "install consul-debug-read cleanup complete!"
   exit
+}
+
+# Function to install a tool
+function install_tool() {
+    local tool_name="$1"
+    local tool_url="${tools[$tool_name]}"
+
+    check_and_install_brew
+    if [ -n "$tool_url" ]; then
+        info "installing $tool_name..."
+        if [[ "$tool_url" =~ .*brew.* ]]; then
+          if ! brew install "$tool_name"; then
+            err "failed to install $tool_name using homebrew | please install manually and reattempt scripted installation"
+          fi
+        else
+          warn "missing $tool_name on local machine ($HOSTNAME) | please install manually and reattempt scripted installation"
+          exit 
+        fi
+    else
+        err "cannot install $tool_name. please install it manually."
+    fi
+}
+
+# Function to check if a tool is installed
+function check_tool_installed() {
+    local tool_name="$1"
+
+    if command -v "$tool_name" >/dev/null 2>&1; then
+        info "$tool_name verified installed => $(which "$tool_name")"
+    else
+        warn "$tool_name not found on local machine ($HOSTNAME)."
+        read -p "install $tool_name now? (y/n): " -n 1 -r
+        if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+            install_tool "$tool_name"
+            if command -v "$tool_name" >/dev/null 2>&1; then
+              info "$tool_name verified installed => $(which "$tool_name")"
+            else
+              err "$tool_name installation failed. please install manually and reattempt scripted installation"
+            fi
+        fi
+    fi
 }
 
 function install_latest_release() {
@@ -46,6 +166,9 @@ function install_latest_release() {
   ! test -f /usr/local/bin/consul-debug-read || {
     debug "install consul-debug-read: removing file named consul-debug-read from /usr/local/bin"
     sudo rm -rf /usr/local/bin/consul-debug-read >/dev/null 2>&1 || true;
+  }
+  go_path_verification || {
+    err "failed to remove $GOPATH/bin/consul-debug-read"
   }
   ! test -d /usr/local/bin/consul-debug-read || {
     debug "install consul-debug-read: removing dir named consul-debug-read from /usr/local/bin"
@@ -73,5 +196,12 @@ function install_latest_release() {
   }
 }
 clear
-install_latest_release
-info "consul-debug-read ${VERSION} installed => /usr/local/bin/consul-debug-read"
+info "starting scripted installation of consul-debug-read cli tool"
+info "verifying prerequisites installed for downloading consul-debug-read"
+# Loop through the tools and check if they are installed
+for tool in "${!tools[@]}"; do
+    check_tool_installed "$tool"
+done
+info "prerequisite verification complete, continuing with installation"
+sudo_prompt && install_latest_release
+info "consul-debug-read version: $(/usr/local/bin/consul-debug-read --version) installed successfully! | (location: /usr/local/bin/consul-debug-read)"
